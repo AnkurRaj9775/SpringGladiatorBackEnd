5e70596257247704faeb59982eb20ec26ed52622
mubhanawat <munish.bhanawat@gmail.com>
2020-08-23 11:48:36 +0530

    wallet structure created

---
+++ EcoBus/src/main/java/com/lti/bridge/WalletDetails.java
@@ -0,0 +1,17 @@
+package com.lti.bridge;
+
+public class WalletDetails {
+
+	
+	private double walletAmount;
+
+	public double getWalletAmount() {
+		return walletAmount;
+	}
+
+	public void setWalletAmount(double walletAmount) {
+		this.walletAmount = walletAmount;
+	}
+	
+	
+}
