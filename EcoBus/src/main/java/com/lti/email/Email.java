19e4f754712f68a1f6de3b332d6a2303c2bfc9db
mubhanawat <munish.bhanawat@gmail.com>
2020-08-24 11:40:36 +0530

    dependency added for email

--- EcoBus/src/main/java/com/lti/email/Email.java
+++ EcoBus/src/main/java/com/lti/email/Email.java
@@ -1,5 +1,4 @@
 package com.lti.email;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
