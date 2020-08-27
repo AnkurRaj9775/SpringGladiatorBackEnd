f1426aca1f678793a947b5f469f92704076b269e
AnkurRaj9775 <Ankur.Raj@lntinfotech.com>
2020-08-27 16:11:00 +0530

    Register Status Message

---
+++ EcoBus/src/main/java/com/lti/bridge/RegisterStatus.java
@@ -0,0 +1,14 @@
+package com.lti.bridge;
+
+public class RegisterStatus {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
